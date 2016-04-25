a1b51e5bd080f276d2a280fb37767b4d5ea2c976
Tristan Jesse <Tristan Jesse>
2016-04-25 14:05:46 -0500

    updated interrupt text

--- SIM800sendText/SIM800sendText.ino
+++ SIM800sendText/SIM800sendText.ino
@@ -1,69 +1,40 @@
 #include <SoftwareSerial.h>
+#include <TinyGPS.h>
  
-//SIM800 TX is connected to Arduino D8
 #define SIM800_TX_PIN 4
- 
-//SIM800 RX is connected to Arduino D7
 #define SIM800_RX_PIN 10
+
+#define SIM_PWR 11
+#define BUTTON_PIN 2
+
+volatile boolean simPower = false;
+volatile boolean interruptTrigger = false;
  
 //Create software serial object to communicate with SIM800
-SoftwareSerial serialSIM800(SIM800_TX_PIN,SIM800_RX_PIN);
+SoftwareSerial simSerial(SIM800_TX_PIN,SIM800_RX_PIN);
  
 void setup() {
-  pinMode(11, OUTPUT);
+  pinMode(SIM_PWR, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+  attachInterrupt(0, btn_ISR, RISING);
   
   //Begin serial comunication with Arduino and Arduino IDE (Serial Monitor)
   Serial.begin(9600);
   while(!Serial);
 
+  //simSerial.begin(9600);
   
-  //Being serial communication witj Arduino and SIM800
-  serialSIM800.begin(9600);
-  delay(1000);
-  digitalWrite(11, LOW);
-  delay(2000);
-  digitalWrite(11, HIGH);
-  delay(3000);
-
-  if(serialSIM800.available()){
-    Serial.print("Reading: \"");
-    while(serialSIM800.available())
-    {
-      char c=serialSIM800.read();
-      Serial.write(c);          
-    }
-    Serial.println("\"");
-  }
-   
-  Serial.println("Setup Complete!");
-  Serial.println("Sending SMS...");
-   
-  //Set SMS format to ASCII
-  sendAndEcho("AT+CMGF=1");
-  delay(1000);
  
-  //Send new SMS command and message number
-  sendAndEcho("AT+CMGS=\"xxxxxxxxx\"");
-  delay(1000);
-   
-  //Send SMS content
-  serialSIM800.write("TEST");
-  delay(1000);
-   
-  //Send Ctrl+Z / ESC to denote SMS message is complete
-  serialSIM800.write((char)26);
-  delay(1000);
-     
-  Serial.println("SMS Sent!");
+  Serial.println("Waiting for Button");
 }
 
 void sendAndEcho(String command) {
-  serialSIM800.println(command);
-  if(serialSIM800.available()){
+  simSerial.println(command);
+  if(simSerial.available()){
     Serial.print("Reading: \"");
-    while(serialSIM800.available())
+    while(simSerial.available())
     {
-      char c=serialSIM800.read();
+      char c=simSerial.read();
       Serial.write(c);          
     }
     Serial.println("\"");
@@ -73,37 +44,58 @@ void sendAndEcho(String command) {
  
 void loop() {
   //Read SIM800 output (if available) and print it in Arduino IDE Serial Monitor
-  /*sendAndEcho("AT");
-
-  sendAndEcho("ATI");
-  sendAndEcho("AT+GSV");
-  sendAndEcho("AT&V");
-  sendAndEcho("AT+CMEE=?");
-  sendAndEcho("ATE0&W");
-  sendAndEcho("AT");
-  sendAndEcho("AT");
-  sendAndEcho("ATE1&W");
-  sendAndEcho("AT");
-  sendAndEcho("AT+CFUN=0");
-  sendAndEcho("AT+IPR?");
-  sendAndEcho("AT+CFUN=0");
-  sendAndEcho("AT+CFUN?");
-
-  //sendAndEcho("AT+CPIN?");
-
-  //sendAndEcho("AT+CSMINS?");
   
-  /*sendAndEcho("AT+CMGF?");
+  if (interruptTrigger) {
+    Serial.println("Button Press");
+    //while (gpsSerial.available()) { gpsSerial.read(); }
+    //gpsSerial.end();
+    simSerial.begin(9600);
+    delay(1000);
 
-  sendAndEcho("AT+CPIN?");
+    simSerial.listen();
 
-  
+    if (!simPower) {
+      digitalWrite(SIM_PWR, LOW);
+      delay(2000);
+      digitalWrite(SIM_PWR, HIGH);
+      delay(3000);
+    
+      if(simSerial.available()){
+        Serial.print("Reading: \"");
+        while(simSerial.available())
+        {
+          char c=simSerial.read();
+          Serial.write(c);          
+        }
+        Serial.println("\"");
+      }
+      simPower = true;
+    }
+    //dataString += locData;
+    Serial.println("Sending SMS...");
+   
+    //Set SMS format to ASCII
+    sendAndEcho("AT+CMGF=1");
+    delay(1000);
+   
+    //Send new SMS command and message number
+    sendAndEcho("AT+CMGS=\"+19728548402\"");
+    delay(1000);
+     
+    //Send SMS content
+    simSerial.print("Test Interrupt");
+    delay(1000);
+     
+    //Send Ctrl+Z / ESC to denote SMS message is complete
+    simSerial.write((char)26);
+    delay(1000);
+       
+    Serial.println("SMS Sent!");
+    interruptTrigger = false;
+    simSerial.end();
+  }
+}
 
-  sendAndEcho("AT+CFUN=?");
-
-  sendAndEcho("AT+CREG?");
-
-  sendAndEcho("AT+COPS?");
-
-  sendAndEcho("AT+COPS=?");
+void btn_ISR() {
+  interruptTrigger = true;
 }
